64c0980f0414735ab3894f5b5256ffe78310c05e
Ali Adel <aliadelali671@gmail.com>
2023-03-28 22:01:40 +0200

    init project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import DisplayTodos from './components/DisplayTodos';
+import Todo from './components/Todo';
+import { motion } from 'framer-motion';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='mt-12 flex flex-col'>
+      <motion.h1
+        initial={{ y: -200 }}
+        animate={{ y: 0 }}
+        transition={{ type: "spring", duration: 0.5 }}
+        whileHover={{ scale: 1.1 }}
+        className='text-4xl p-6 inline text-center mb-8 head'
+      >
+        Todo App
+      </motion.h1>
+      <motion.div
+        initial={{ y: 1000 }}
+        animate={{ y: 0 }}
+        transition={{ type: "spring", duration: 1 }}
+      >
+        <Todo />
+        <DisplayTodos />
+      </motion.div>
     </div>
   );
 }
---
+++ src/components/DisplayTodos.js
@@ -0,0 +1,85 @@
+import { Button } from '@material-tailwind/react';
+import React, { useState } from 'react'
+import { connect } from 'react-redux';
+import { addTodos, completeTodos, removeTodos, updateTodos } from '../state/reducer';
+import TodoItem from './TodoItem';
+import { AnimatePresence, motion } from 'framer-motion';
+
+const mapStateToProps = (state) => {
+    return {
+        todo: state,
+    };
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        addTodo: (obj) => dispatch(addTodos(obj)),
+        removeTodo: (id) => dispatch(removeTodos(id)),
+        updateTodo: (obj) => dispatch(updateTodos(obj)),
+        completeTodo: (id) => dispatch(completeTodos(id)),
+    };
+};
+
+const DisplayTodos = (props) => {
+    const [sort, setSort] = useState("active")
+    return (
+        <div className='mt-12 flex flex-col items-center'>
+            <div className='mb-8'>
+                <motion.button whileHover={{scale: 1.1}} whileTap={{scale: 0.9}} className='bg-slate-200 text-black rounded-full py-3 px-5 ml-4 cursor-pointer border-none focus:outline-none' onClick={() => setSort("active")}>Active</motion.button>
+                <motion.button whileHover={{scale: 1.1}} whileTap={{scale: 0.9}} className='bg-slate-200 text-black rounded-full py-3 px-5 ml-4 cursor-pointer border-none focus:outline-none' onClick={() => setSort("completed")}>Completed</motion.button>
+                <motion.button whileHover={{scale: 1.1}} whileTap={{scale: 0.9}} className='bg-slate-200 text-black rounded-full py-3 px-5 ml-4 cursor-pointer border-none focus:outline-none' onClick={() => setSort("all")}>All</motion.button>
+            </div>
+            <ul className='list-none flex self-start flex-wrap ml-10 lg:ml-20'>
+                <AnimatePresence>
+                    {props.todo.length > 0 && sort === "active"
+                        ? props.todo.map((item) => {
+                            return (
+                                item.completed === false && (
+                                    <TodoItem
+                                        key={item.id}
+                                        item={item}
+                                        removeTodo={props.removeTodo}
+                                        updateTodo={props.updateTodo}
+                                        completeTodo={props.completeTodo}
+                                    />
+                                )
+                            );
+                        })
+                        : null}
+
+                    {props.todo.length > 0 && sort === "completed"
+                        ? props.todo.map((item) => {
+                            return (
+                                item.completed === true && (
+                                    <TodoItem
+                                        key={item.id}
+                                        item={item}
+                                        removeTodo={props.removeTodo}
+                                        updateTodo={props.updateTodo}
+                                        completeTodo={props.completeTodo}
+                                    />
+                                )
+                            );
+                        })
+                        : null}
+
+                    {props.todo.length > 0 && sort === "all"
+                        ? props.todo.map((item) => {
+                            return (
+                                <TodoItem
+                                    key={item.id}
+                                    item={item}
+                                    removeTodo={props.removeTodo}
+                                    updateTodo={props.updateTodo}
+                                    completeTodo={props.completeTodo}
+                                />
+                            );
+                        })
+                        : null}
+                </AnimatePresence>
+            </ul>
+        </div>
+    )
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(DisplayTodos);
